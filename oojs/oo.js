381f049a3a45ec01feee1dd2fab6d20e91f40266
guilherme <guguiejo@gmail.com>
2023-09-09 09:23:46 -0300

    adicionado instancia

--- oojs/oo.js
+++ oojs/oo.js
@@ -1,22 +1,26 @@
-
 function Moto(marca, modelo, cilindrada) {
     this.marca = marca;
     this.modelo = modelo;
     this.cilindrada = cilindrada;
 
-
     this.ligar = function () {
-    console.log(`A ${this.marca} ${this.modelo} (${this.cilindrada}cc) está ligada.`);
+        console.log(`A ${this.marca} ${this.modelo} (${this.cilindrada}cc) está ligada.`);
     };
 
     this.acelerar = function () {
-    console.log(`A ${this.marca} ${this.modelo} está acelerando.`);
+        console.log(`A ${this.marca} ${this.modelo} está acelerando.`);
     };
 }
 
-
 const minhaMoto = new Moto('Ducati', 'Monster', 821);
+const segundaMoto = new Moto('Honda', 'CBR1000RR', 1000);
+const terceiraMoto = new Moto('Yamaha', 'YZF-R6', 600);
 
+minhaMoto.ligar();
+minhaMoto.acelerar();
 
-minhaMoto.ligar(); 
-minhaMoto.acelerar(); 
+segundaMoto.ligar();
+segundaMoto.acelerar();
+
+terceiraMoto.ligar();
+terceiraMoto.acelerar();
